0a328c03f5d0ce2f920f0ba004f5fb51119df0bb
Thiago Nogueira <thiagotn@gmail.com>
2022-07-05 07:58:14 -0300

    move SUT creation to a setup method

--- lib/domain/usecases/authentication.dart
+++ lib/domain/usecases/authentication.dart
@@ -3,8 +3,17 @@ import 'package:flutter/widgets.dart';
 import '../entities/entities.dart';
 
 abstract class Authentication {
-  Future<AccountEntity> auth({
-    @required String email,
-    @required String password,
+  Future<AccountEntity> auth(
+    AuthenticationParams params,
+  );
+}
+
+class AuthenticationParams {
+  final String email;
+  final String secret;
+
+  AuthenticationParams({
+    @required this.email,
+    @required this.secret,
   });
 }
