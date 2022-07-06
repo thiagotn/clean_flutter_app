982710ec74a721c4e341e3acd267c0632bcc90a6
Thiago Nogueira <thiagotn@gmail.com>
2022-07-06 15:55:18 -0300

    ensure HttpAdapter calls post without body

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -20,10 +20,11 @@ class HttpAdapter {
       'content-type': 'application/json',
       'accept': 'application/json',
     };
+    final jsonBody = body != null ? jsonEncode(body) : null;
     await client.post(
       Uri.parse(url),
       headers: headers,
-      body: jsonEncode(body),
+      body: jsonBody,
     );
   }
 }
@@ -55,6 +56,17 @@ void main() {
           'accept': 'application/json',
         },
         body: '{"any_key":"any_value"}',
+      ));
+    });
+
+    test('Should call post without body', () async {
+      await sut.request(
+        url: url,
+        method: 'post',
+      );
+      verify(client.post(
+        any,
+        headers: anyNamed('headers'),
       ));
     });
   });
