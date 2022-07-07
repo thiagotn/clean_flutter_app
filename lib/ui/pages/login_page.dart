b7b74de29973fbda70cd27751bd91aeaacc44290
Thiago Nogueira <thiagotn@gmail.com>
2022-07-07 18:31:10 -0300

    refactor: extracting components from LoginPage

--- lib/ui/pages/login_page.dart
+++ lib/ui/pages/login_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../components/components.dart';
+
 class LoginPage extends StatelessWidget {
   const LoginPage({Key key}) : super(key: key);
 
@@ -10,41 +12,8 @@ class LoginPage extends StatelessWidget {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            Container(
-              height: 240,
-              margin: const EdgeInsets.only(
-                bottom: 32,
-              ),
-              decoration: BoxDecoration(
-                gradient: LinearGradient(
-                  begin: Alignment.topRight,
-                  end: Alignment.bottomLeft,
-                  colors: [
-                    Theme.of(context).primaryColorLight,
-                    Theme.of(context).primaryColorDark,
-                  ],
-                ),
-                boxShadow: const [
-                  BoxShadow(
-                    offset: Offset(0, 0),
-                    spreadRadius: 0,
-                    blurRadius: 4,
-                    color: Colors.black,
-                  ),
-                ],
-                borderRadius: const BorderRadius.only(
-                  bottomLeft: Radius.circular(80),
-                ),
-              ),
-              child: const Image(
-                image: AssetImage('lib/ui/assets/logo.png'),
-              ),
-            ),
-            Text(
-              'Login'.toUpperCase(),
-              textAlign: TextAlign.center,
-              style: Theme.of(context).textTheme.headline1,
-            ),
+            const LoginHeader(),
+            const Headline1(text: 'Login'),
             Padding(
               padding: const EdgeInsets.all(32),
               child: Form(
