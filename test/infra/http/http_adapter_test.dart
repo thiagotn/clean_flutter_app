4247823e84424624974e283c3e2ccc86b9625711
Thiago Nogueira <thiagotn@gmail.com>
2022-07-07 15:45:32 -0300

    ensure HttpAdapter throws ServerError if post throws

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -39,6 +39,8 @@ void main() {
       mockRequest().thenAnswer((_) async => Response(body, statusCode));
     }
 
+    void mockError() => mockRequest().thenThrow(Exception());
+
     setUp(() {
       mockResponse(200);
     });
@@ -170,6 +172,17 @@ void main() {
 
     test('Should return ServerError if post returns 500', () async {
       mockResponse(500);
+
+      final future = sut.request(
+        url: url,
+        method: 'post',
+      );
+
+      expect(future, throwsA(HttpError.serverError));
+    });
+
+    test('Should return ServerError if post throws Exception', () async {
+      mockError();
 
       final future = sut.request(
         url: url,
