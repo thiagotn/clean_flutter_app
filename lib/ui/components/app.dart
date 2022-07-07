a07ffe45f1df5aa1a697f400946a618c64cf00dc
Thiago Nogueira <thiagotn@gmail.com>
2022-07-07 18:22:11 -0300

    add basic layout for LoginPage

--- lib/ui/components/app.dart
+++ lib/ui/components/app.dart
@@ -7,10 +7,49 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    const primaryColor = Color.fromRGBO(136, 14, 79, 1);
+    const primaryColorDark = Color.fromRGBO(96, 0, 39, 1);
+    const primaryColorLight = Color.fromRGBO(188, 71, 123, 1);
+
+    return MaterialApp(
       title: '4Dev',
       debugShowCheckedModeBanner: false,
-      home: LoginPage(),
+      theme: ThemeData(
+        primaryColor: primaryColor,
+        primaryColorDark: primaryColorDark,
+        primaryColorLight: primaryColorLight,
+        backgroundColor: Colors.white,
+        textTheme: const TextTheme(
+          headline1: TextStyle(
+            fontSize: 30,
+            fontWeight: FontWeight.bold,
+            color: primaryColorDark,
+          ),
+        ),
+        inputDecorationTheme: const InputDecorationTheme(
+          enabledBorder: UnderlineInputBorder(
+            borderSide: BorderSide(color: primaryColorLight),
+          ),
+          focusedBorder: UnderlineInputBorder(
+            borderSide: BorderSide(color: primaryColor),
+          ),
+          alignLabelWithHint: true,
+        ),
+        buttonTheme: ButtonThemeData(
+          colorScheme: const ColorScheme.light(primary: primaryColor),
+          buttonColor: primaryColor,
+          splashColor: primaryColorLight,
+          padding: const EdgeInsets.symmetric(
+            vertical: 10,
+            horizontal: 20,
+          ),
+          textTheme: ButtonTextTheme.primary,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(20),
+          ),
+        ),
+      ),
+      home: const LoginPage(),
     );
   }
 }
--- lib/ui/pages/login_page.dart
+++ lib/ui/pages/login_page.dart
@@ -8,46 +8,89 @@ class LoginPage extends StatelessWidget {
     return Scaffold(
       body: SingleChildScrollView(
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            const Image(
-              image: AssetImage('lib/ui/assets/logo.png'),
-            ),
-            Text('Login'.toUpperCase()),
-            Form(
-              child: Column(
-                children: [
-                  TextFormField(
-                    decoration: const InputDecoration(
-                      labelText: 'Email',
-                      icon: Icon(
-                        Icons.email,
-                      ),
-                    ),
-                    keyboardType: TextInputType.emailAddress,
-                  ),
-                  TextFormField(
-                    decoration: const InputDecoration(
-                      labelText: 'Password',
-                      icon: Icon(
-                        Icons.lock,
-                      ),
-                    ),
-                    keyboardType: TextInputType.visiblePassword,
-                  ),
-                  TextButton(
-                    onPressed: () {},
-                    child: Text(
-                      'Entrar'.toUpperCase(),
-                    ),
-                  ),
-                  TextButton.icon(
-                    onPressed: () {},
-                    label: const Text(
-                      'Criar Conta',
-                    ),
-                    icon: const Icon(Icons.person),
+            Container(
+              height: 240,
+              margin: const EdgeInsets.only(
+                bottom: 32,
+              ),
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.topRight,
+                  end: Alignment.bottomLeft,
+                  colors: [
+                    Theme.of(context).primaryColorLight,
+                    Theme.of(context).primaryColorDark,
+                  ],
+                ),
+                boxShadow: const [
+                  BoxShadow(
+                    offset: Offset(0, 0),
+                    spreadRadius: 0,
+                    blurRadius: 4,
+                    color: Colors.black,
                   ),
                 ],
+                borderRadius: const BorderRadius.only(
+                  bottomLeft: Radius.circular(80),
+                ),
+              ),
+              child: const Image(
+                image: AssetImage('lib/ui/assets/logo.png'),
+              ),
+            ),
+            Text(
+              'Login'.toUpperCase(),
+              textAlign: TextAlign.center,
+              style: Theme.of(context).textTheme.headline1,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(32),
+              child: Form(
+                child: Column(
+                  children: [
+                    TextFormField(
+                      decoration: InputDecoration(
+                        labelText: 'Email',
+                        icon: Icon(
+                          Icons.email,
+                          color: Theme.of(context).primaryColorLight,
+                        ),
+                      ),
+                      keyboardType: TextInputType.emailAddress,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(
+                        top: 8,
+                        bottom: 32,
+                      ),
+                      child: TextFormField(
+                        decoration: InputDecoration(
+                          labelText: 'Password',
+                          icon: Icon(
+                            Icons.lock,
+                            color: Theme.of(context).primaryColorLight,
+                          ),
+                        ),
+                        keyboardType: TextInputType.visiblePassword,
+                      ),
+                    ),
+                    TextButton(
+                      onPressed: () {},
+                      child: Text(
+                        'Entrar'.toUpperCase(),
+                      ),
+                    ),
+                    TextButton.icon(
+                      onPressed: () {},
+                      label: const Text(
+                        'Criar Conta',
+                      ),
+                      icon: const Icon(Icons.person),
+                    ),
+                  ],
+                ),
               ),
             )
           ],
