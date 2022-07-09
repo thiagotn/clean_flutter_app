2058a1f28349e8a8d8dd8091a9aebc0ff0b32439
Thiago Nogueira <thiagotn@gmail.com>
2022-07-09 20:30:59 -0300

    move EmailInput to its own file

--- lib/ui/pages/login/login_page.dart
+++ lib/ui/pages/login/login_page.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import '../../components/components.dart';
-import 'login_presenter.dart';
+import 'components/components.dart';
+import '../login/login_presenter.dart';
 
 class LoginPage extends StatefulWidget {
   final LoginPresenter presenter;
@@ -40,75 +42,61 @@ class _LoginPageState extends State<LoginPage> {
                 const Headline1(text: 'Login'),
                 Padding(
                   padding: const EdgeInsets.all(32),
-                  child: Form(
-                    child: Column(
-                      children: [
-                        StreamBuilder<String>(
-                          stream: widget.presenter.emailErrorStream,
-                          builder: (context, snapshot) {
-                            return TextFormField(
-                              decoration: InputDecoration(
-                                labelText: 'Email',
-                                icon: Icon(
-                                  Icons.email,
-                                  color: Theme.of(context).primaryColorLight,
-                                ),
-                                errorText: snapshot.data?.isEmpty == true
-                                    ? null
-                                    : snapshot.data,
-                              ),
-                              keyboardType: TextInputType.emailAddress,
-                              onChanged: widget.presenter.validateEmail,
-                            );
-                          },
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.only(
-                            top: 8,
-                            bottom: 32,
-                          ),
-                          child: StreamBuilder<String>(
-                              stream: widget.presenter.passwordErrorStream,
-                              builder: (context, snapshot) {
-                                return TextFormField(
-                                  decoration: InputDecoration(
-                                    labelText: 'Senha',
-                                    icon: Icon(
-                                      Icons.lock,
-                                      color:
-                                          Theme.of(context).primaryColorLight,
+                  child: Provider<LoginPresenter>(
+                    create: (_) => widget.presenter,
+                    child: Form(
+                      child: Column(
+                        children: [
+                          const EmailInput(),
+                          Padding(
+                            padding: const EdgeInsets.only(
+                              top: 8,
+                              bottom: 32,
+                            ),
+                            child: StreamBuilder<String>(
+                                stream: widget.presenter.passwordErrorStream,
+                                builder: (context, snapshot) {
+                                  return TextFormField(
+                                    decoration: InputDecoration(
+                                      labelText: 'Senha',
+                                      icon: Icon(
+                                        Icons.lock,
+                                        color:
+                                            Theme.of(context).primaryColorLight,
+                                      ),
+                                      errorText: snapshot.data?.isEmpty == true
+                                          ? null
+                                          : snapshot.data,
                                     ),
-                                    errorText: snapshot.data?.isEmpty == true
-                                        ? null
-                                        : snapshot.data,
-                                  ),
-                                  keyboardType: TextInputType.text,
-                                  obscureText: true,
-                                  onChanged: widget.presenter.validatePassword,
-                                );
-                              }),
-                        ),
-                        StreamBuilder<bool>(
-                          stream: widget.presenter.isFormValidStream,
-                          builder: (context, snapshot) {
-                            return TextButton(
-                              onPressed: snapshot.data == true
-                                  ? widget.presenter.auth
-                                  : null,
-                              child: Text(
-                                'Entrar'.toUpperCase(),
-                              ),
-                            );
-                          },
-                        ),
-                        TextButton.icon(
-                          onPressed: () {},
-                          label: const Text(
-                            'Criar Conta',
+                                    keyboardType: TextInputType.text,
+                                    obscureText: true,
+                                    onChanged:
+                                        widget.presenter.validatePassword,
+                                  );
+                                }),
                           ),
-                          icon: const Icon(Icons.person),
-                        ),
-                      ],
+                          StreamBuilder<bool>(
+                            stream: widget.presenter.isFormValidStream,
+                            builder: (context, snapshot) {
+                              return TextButton(
+                                onPressed: snapshot.data == true
+                                    ? widget.presenter.auth
+                                    : null,
+                                child: Text(
+                                  'Entrar'.toUpperCase(),
+                                ),
+                              );
+                            },
+                          ),
+                          TextButton.icon(
+                            onPressed: () {},
+                            label: const Text(
+                              'Criar Conta',
+                            ),
+                            icon: const Icon(Icons.person),
+                          ),
+                        ],
+                      ),
                     ),
                   ),
                 )
