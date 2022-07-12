a7a01631b3213e8504bb33c04fd0f0d045b633a5
Thiago Teixeira Nogueira <tnogueira@uolinc.com>
2022-07-12 20:11:48 -0300

    add ValidationBuilder

--- lib/main/factories/pages/login/login_validation_factory.dart
+++ lib/main/factories/pages/login/login_validation_factory.dart
@@ -1,15 +1,15 @@
 import '../../../../presentation/protocols/protocols.dart';
 import '../../../../validation/validators/validators.dart';
 import '../../../../validation/protocols/protocols.dart';
+import '../../../builders/builders.dart';
 
 Validation makeLoginValidation() {
   return ValidationComposite(makeLoginValidations());
 }
 
 List<FieldValidation> makeLoginValidations() {
-  return const [
-    RequiredFieldValidation('email'),
-    EmailValidation('email'),
-    RequiredFieldValidation('password')
+  return [
+    ...ValidationBuilder.field('email').required().email().build(),
+    ...ValidationBuilder.field('password').required().build(),
   ];
 }
