61d664f2d4e4023708eac14bde104f8fc2c08f80
Thiago Teixeira Nogueira <tnogueira@uolinc.com>
2022-07-15 20:18:46 -0300

    ensure LocalLoadCurrentAccount returns an account on sucess

--- requirements/use_cases/local_load_current_account.md
+++ requirements/use_cases/local_load_current_account.md
@@ -2,7 +2,7 @@
 
 > ## Caso de sucesso
 1. ✅ Sistema solicita o token de acesso do usuário gravado no Cache Seguro
-2. Sistema retorna os dados da conta do usuário
+2. ✅ Sistema retorna os dados da conta do usuário
 
 > ## Exceção - Falha ao gravar no cache
 1. Sistema retorna uma mensagem de erro inesperado
--- test/data/usecases/load_current_account/local_load_current_account_test.dart
+++ test/data/usecases/load_current_account/local_load_current_account_test.dart
@@ -1,19 +1,25 @@
+import 'package:faker/faker.dart';
 import 'package:flutter/widgets.dart';
-import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
+import 'package:flutter_test/flutter_test.dart';
 
-class LocalLoadCurrentAccount {
+import 'package:clean_flutter_app/domain/entities/entities.dart';
+import 'package:clean_flutter_app/domain/usecases/usecases.dart';
+
+class LocalLoadCurrentAccount implements LoadCurrentAccount {
   final FetchSecureCacheStorage fetchSecureCacheStorage;
 
   LocalLoadCurrentAccount({@required this.fetchSecureCacheStorage});
 
-  Future<void> load() async {
-    await fetchSecureCacheStorage.fetchSecure('token');
+  @override
+  Future<AccountEntity> load() async {
+    final token = await fetchSecureCacheStorage.fetchSecure('token');
+    return AccountEntity(token);
   }
 }
 
 abstract class FetchSecureCacheStorage {
-  Future<void> fetchSecure(String key);
+  Future<String> fetchSecure(String key);
 }
 
 class FetchSecureCacheStorageSpy extends Mock
@@ -22,16 +28,30 @@ class FetchSecureCacheStorageSpy extends Mock
 void main() {
   LocalLoadCurrentAccount sut;
   FetchSecureCacheStorage fetchSecureCacheStorage;
+  String token;
+
+  void mockFetchSecure() {
+    when(fetchSecureCacheStorage.fetchSecure(any))
+        .thenAnswer((_) async => token);
+  }
 
   setUp(() {
     fetchSecureCacheStorage = FetchSecureCacheStorageSpy();
     sut = LocalLoadCurrentAccount(
         fetchSecureCacheStorage: fetchSecureCacheStorage);
+    token = faker.guid.guid();
+    mockFetchSecure();
   });
 
   test('Should call FetchSecureCacheStorage with correct values', () async {
     await sut.load();
 
     verify(fetchSecureCacheStorage.fetchSecure('token'));
+  });
+
+  test('Should return an AccountEntity', () async {
+    final account = await sut.load();
+
+    expect(account, AccountEntity(token));
   });
 }
